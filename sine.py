d142132b61b6761daaa557b97a5e78ca2abcba9c
sameer <bvks2020sameer@gmail.com>
2022-03-08 23:11:00 +0530

    first commit

---
+++ sine.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+from math import *
+x =[x for x in range(361)]
+sine = [sin(x*2*pi/180) for x in range(361)]
+plt.title("sine wave")
+plt.pie(x,sine)
+plt.show()
